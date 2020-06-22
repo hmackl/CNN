205dc63e1b233efacfabdefafec79478c2807988
Harry Macklewain-Lawrence <14MacklH@staidans.co.uk>
2020-06-22 03:24:07 +0100

    Convolver Fixed and Tested

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 std::unique_ptr<Gdiplus::Bitmap> convolver();
 
-VOID process(HDC hdc) 
+VOID process(HDC hdc)
 {
     Gdiplus::Graphics window(hdc);
     Gdiplus::Bitmap *out = convolver().release();
@@ -25,22 +25,22 @@ std::unique_ptr<Gdiplus::Bitmap> convolver()
     for (int y = -1; y < (int)inSize[1] - 1; y++)
         for (int x = -1; x < (int)inSize[0] - 1; x++)
         {
-            int convolved[3];
+            int convolved = 0;
             for (int b = 0; b < 3; b++)
                 for (int a = 0; a < 3; a++)
                 {
                     Gdiplus::Color inColor(0, 0, 0);
                     int xy[2] = {x + a, y + b};
-                    if (xy[0] >= 0 && xy[1] >= 0 && xy[0] <= inSize[0] && xy[1] <= inSize[1])
+                    if (xy[0] >= 0 && xy[1] >= 0 && xy[0] < inSize[0] && xy[1] < inSize[1])
                     {
                         in.GetPixel(xy[0], xy[1], &inColor);
                     }
-                    int rgb[3] = {inColor.GetR(), inColor.GetG(), inColor.GetB()};
-                    convolved[0] += (inColor.GetR() * kernel[a][b]) + bias;
-                    convolved[1] += (inColor.GetG() * kernel[a][b]) + bias;
-                    convolved[2] += (inColor.GetB() * kernel[a][b]) + bias;
+                    convolved += (inColor.GetR() * kernel[a][b]) +
+                                 (inColor.GetG() * kernel[a][b]) +
+                                 (inColor.GetB() * kernel[a][b]) +
+                                 bias;
                 }
-            Gdiplus::Color outColor(convolved[0], convolved[1], convolved[2]);
+            Gdiplus::Color outColor(convolved, convolved, convolved);
             out->SetPixel(x + 1, y + 1, outColor);
         }
     return (out);
